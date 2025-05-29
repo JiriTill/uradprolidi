0caa1769dbc1ef701d8221ec809ea84e5096b0e3
JiriTill <jirik.till@gmail.com>
2025-05-29 16:14:29 +0200

    Update translate.js

--- api/translate.js
+++ api/translate.js
@@ -54,7 +54,7 @@ export default async function handler(req, res) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        model: 'gpt-4-vision-preview',
+        model: 'gpt-4o',
         messages,
         max_tokens: 1500,
       }),
