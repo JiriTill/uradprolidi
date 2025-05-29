80b901dd388e36caee555a50924c1f21878b9294
JiriTill <jirik.till@gmail.com>
2025-05-29 13:34:03 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';       // Import komponenty App
+import App from './app.jsx';       // Import komponenty App
 import './style.css';              // Import stylů (např. Tailwind)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
