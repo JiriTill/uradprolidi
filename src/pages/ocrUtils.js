8c4a916cab9522ccf3fbf1ce30c69a0a6fae9f99
JiriTill <jirik.till@gmail.com>
2025-05-29 15:13:17 +0200

    Update ocrUtils.js

--- src/pages/ocrUtils.js
+++ src/pages/ocrUtils.js
@@ -1,17 +1,6 @@
 import Tesseract from 'tesseract.js';
 
-export const recognizeImageText = async (file) => {
-  return new Promise((resolve, reject) => {
-    Tesseract.recognize(
-      file,
-      'eng',
-      {
-        logger: (m) => console.log(m), // můžeš smazat pro produkci
-      }
-    ).then(({ data: { text } }) => {
-      resolve(text);
-    }).catch((err) => {
-      reject(err);
-    });
-  });
-};
+export async function recognizeTextFromImage(imageFile) {
+  const { data: { text } } = await Tesseract.recognize(imageFile, 'eng');
+  return text;
+}
