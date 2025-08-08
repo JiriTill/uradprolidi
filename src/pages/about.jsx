ed40cc80dfaab655690ff18e6af689890dceec7b
Jiri Till <jirik.till@gmail.com>
2025-08-08 10:31:04 +0200

    Update about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -39,6 +39,7 @@ export default function About() {
         <Link
           to="/"
           className="inline-block mt-6 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+          onClick={scrollToTop}
         >
           Zpět k překladu do člověčiny
         </Link>
